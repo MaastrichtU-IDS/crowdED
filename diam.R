d00cc034ac5218cbb0962bf8584d62690d561653
Pedro V <hdez.serrano@gmail.com>
2017-12-01 18:31:09 +0100

    plots variables

--- diam.R
+++ diam.R
@@ -7,17 +7,18 @@ nms <- names(diamonds)
 setwd("~/crowdsourcing-task-optimization")
 df.simulations <- read.csv('df_simulations.csv', sep=',', encoding="utf-8")
 
-
+#names <- names(df.simulations)
+#max(df.simulations$simulation)
 
 ui <- fluidPage(
   
   headerPanel("Crowdsourcing"),
   sidebarPanel(
-    sliderInput('sampleSize', 'Sample Size', min = 1, max = nrow(diamonds),
-                value = 1000, step = 500, round = 0),
-    selectInput('x', 'X', choices = nms, selected = "x"),
-    selectInput('y', 'Y', choices = nms, selected = "y"),
-    selectInput('color', 'Color', choices = nms, selected = "clarity"),
+    sliderInput('iterations', 'Iterations', min = 1, max = max(df.simulations$simulation),
+                value = 2, step = 1, round = 0),
+    selectInput('x', 'Workers', choices = names, selected = "workers"),
+    selectInput('y', '% Consensus', choices = names, selected = "accuracy"),
+    #selectInput('color', 'Color', choices = nms, selected = "clarity"),
     
     #selectInput('facet_row', 'Facet Row', c(None = '.', nms), selected = "clarity"),
     #selectInput('facet_col', 'Facet Column', c(None = '.', nms)),
@@ -33,14 +34,14 @@ server <- function(input, output) {
   
   #add reactive data information. Dataset = built in diamonds data
   dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize),]
+    with(df.simulations, df.simulations[simulation == input$iterations, ])
   })
   
   output$trendPlot <- renderPlotly({
     
     # build graph with ggplot syntax
-    p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
-      geom_point()
+    p <- ggplot(dataset(), aes_string(x = input$x, y = input$y)) +#, color = input$color)) + 
+      geom_point() +  geom_line()
     
     # if at least one facet column/row is specified, add it
     #facets <- paste(input$facet_row, '~', input$facet_col)
