003ae31e71172f7502fcdd3f1c391bc0212aab9f
Pedro V <hdez.serrano@gmail.com>
2018-01-24 10:15:48 +0100

    crowded method tested

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='crowdED',
+      version='0.1',
+      description='Crowd Simulation',
+      url='https://github.com/pedrohserrano/crowdED',
+      author='Pedro Hernandez Serrano',
+      author_email='p.hernandezserrano@maastrichtuniversity.nl',
+      license='MIT',
+      packages=['crowded'],
+      zip_safe=False)
