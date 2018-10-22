131ff640dc0ce48b35005c3a768543e22654b2f7
Pedro V <hdez.serrano@gmail.com>
2018-10-22 21:21:46 +0200

    shiny migrated

--- crowdapp/global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(shinydashboard)
-library(ggthemes)
-library(plotly)
-
-
-#ideal <- read.csv("data/UN_IdealPoints.csv", stringsAsFactors = F)
-tabla <- read.csv("data/df_simulationsp_train_tasks1.csv", stringsAsFactors = F)
-tabla1 <- read.csv("data/df_simulationsworkers_per_task21.csv", stringsAsFactors = F)
